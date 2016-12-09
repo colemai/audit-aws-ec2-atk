48b59ad59127b818423ea6cc56426d06702004b2
123prove123 <123prove123@gmail.com>
2016-12-09 16:33:36 +0600

    test1

--- services/config.rb
+++ services/config.rb
@@ -80,7 +80,7 @@ coreo_uni_util_jsrunner "ec2-runner-advise-no-tags-older-than-kill-all-script" d
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.1.3"
+                   :version => "1.1.5"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
@@ -89,9 +89,27 @@ coreo_uni_util_jsrunner "ec2-runner-advise-no-tags-older-than-kill-all-script" d
                 "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.number_ignored_violations",
                 "violations": COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-samples.report}'
   function <<-EOH
+const JSON = json_input;
+const NO_OWNER_EMAIL = "${AUDIT_AWS_EC2_ATK_ALERT_TO_KILL_RECIPIENT}";
+const OWNER_TAG = "${AUDIT_AWS_EC2_ATK_OWNER_TAG}";
+const AUDIT_NAME = 'ec2-samples';
+const IS_KILL_SCRIPTS_SHOW = true;
+const EC2_LOGIC = "${AUDIT_AWS_EC2_ATK_TAG_LOGIC}"; // you can choose 'and' or 'or';
+const EXPECTED_TAGS = [${AUDIT_AWS_EC2_ATK_EXPECTED_TAGS}];
+
+const VARIABLES = {
+    'NO_OWNER_EMAIL': NO_OWNER_EMAIL,
+    'OWNER_TAG': OWNER_TAG,
+    'AUDIT_NAME': AUDIT_NAME,
+    'IS_KILL_SCRIPTS_SHOW': IS_KILL_SCRIPTS_SHOW,
+    'EC2_LOGIC': EC2_LOGIC,
+    'EXPECTED_TAGS': EXPECTED_TAGS
+};
+
+
 const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
-const AuditCloudtrail = new CloudCoreoJSRunner(json_input, true, "${AUDIT_AWS_EC2_ATK_ALERT_TO_KILL_RECIPIENT}", "${AUDIT_AWS_EC2_ATK_OWNER_TAG}", 'ec2-samples');
-const HTMLKillScripts = AuditCloudtrail.getHTMLKillScripts();
+const AuditEC2ATK = new CloudCoreoJSRunner(JSON, VARIABLES);
+const HTMLKillScripts = AuditEC2ATK.getHTMLKillScripts();
 callback(HTMLKillScripts)
   EOH
 end
