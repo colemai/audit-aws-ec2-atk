ccb9868cbb392cad11a7f0faea43eee39982debc
123prove123 <123prove123@gmail.com>
2016-12-12 08:47:48 +0600

    update jsrunner version

--- services/config.rb
+++ services/config.rb
@@ -33,7 +33,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-ec2-samples" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.1.5"
+                   :version => "1.1.6"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
@@ -80,7 +80,7 @@ coreo_uni_util_jsrunner "ec2-runner-advise-no-tags-older-than-kill-all-script" d
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.1.5"
+                   :version => "1.1.6"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
