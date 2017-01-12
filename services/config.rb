9aa1e928fdd44ad71059c849be9515ea08f20cb5
123prove123 <123prove123@gmail.com>
2017-01-12 08:25:37 +0600

    test 1

--- services/config.rb
+++ services/config.rb
@@ -34,7 +34,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-ec2-atk" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.4.4"
+                   :version => "1.4.5"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
@@ -200,39 +200,12 @@ callback(HTMLKillScripts);
   EOH
 end
 
-coreo_uni_util_jsrunner "violations-ec2-atk" do
-  action :run
-  data_type "json"
-  packages([
-               {
-                   :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.3.9"
-               }       ])
-  json_input 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-ec2-atk.violations'
-  function <<-EOH
-callback(json_input);
-  EOH
-end
 
 coreo_uni_util_variables "update-advisor-output" do
   action :set
   variables([
        {'COMPOSITE::coreo_aws_advisor_ec2.advise-ec2-atk.report.violations' => 'COMPOSITE::coreo_uni_util_jsrunner.violations-ec2-atk.return'}
       ])
-end
-
-coreo_uni_util_jsrunner "notifiers-ec2-atk" do
-  action :run
-  data_type "json"
-  packages([
-               {
-                   :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.3.9"
-               }       ])
-  json_input 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-ec2-atk.notifiers'
-  function <<-EOH
-callback(json_input);
-  EOH
 end
 
 # Send ec2-atk for email
@@ -289,7 +262,7 @@ coreo_uni_util_jsrunner "ec2-runner-advise-no-tags-older-than-kill-all-script" d
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.4.4"
+                   :version => "1.4.5"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
@@ -445,10 +418,7 @@ const VARIABLES = {
 
 const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
 const AuditEC2ATK = new CloudCoreoJSRunner(JSON_INPUT, VARIABLES);
-const notifiers = JSON.stringify(AuditEC2ATK.getNotifiers());
-const violations = JSON.stringify(AuditEC2ATK.getJSONForAuditPanel());
-const HTMLKillScripts = AuditEC2ATK.getHTMLKillScripts(); //TODO: scripts are returned as string. This should be fixed and used insted of last jsrunner
-
+const HTMLKillScripts = AuditEC2ATK.getHTMLKillScripts(); 
 callback(HTMLKillScripts)
   EOH
 end
