2bbd6f036a99b00e21c22aef000e93adef744512
colemai <colemai@tcd.ie>
2017-02-16 13:17:40 -0800

    variation with JSON input

--- services/config.rb
+++ services/config.rb
@@ -316,13 +316,6 @@ callback(rollup_string);
   EOH
 end
 
-coreo_uni_util_variables "update-rule-runner" do
-   action :set
-   variables([
-                 {'COMPOSITE::coreo_aws_rule_runner_ec2.advise-ec2-atk.report' => 'COMPOSITE::coreo_uni_util_jsrunner.ec2-runner-advise-no-tags-older-than-kill-all-script'}
-             ])
- end
-
 coreo_uni_util_notify "advise-atk-rollup" do
   action :${AUDIT_AWS_EC2_ATK_ROLLUP_REPORT}
   type 'email'
@@ -471,6 +464,13 @@ const HTMLKillScripts = AuditEC2ATK.getHTMLKillScripts();
 callback(HTMLKillScripts)
   EOH
 end
+
+coreo_uni_util_variables "update-rule-runner" do
+   action :set
+   variables([
+                 {'COMPOSITE::coreo_aws_rule_runner_ec2.advise-ec2-atk.report' => 'COMPOSITE::coreo_uni_util_jsrunner.ec2-runner-advise-no-tags-older-than-kill-all-script.JSON_INPUT'}
+             ])
+ end
 
 coreo_uni_util_notify "advise-ec2-notify-no-tags-older-than-kill-all-script" do
   action :${AUDIT_AWS_EC2_ATK_SHOWN_KILL_SCRIPTS}
