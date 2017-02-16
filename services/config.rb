64062d14c26aafbf243ca417e8a03c8dcdb7393c
Michael Pautov <123prove123@gmail.com>
2017-02-16 15:09:29 +0600

    Created planwide and set results

--- services/config.rb
+++ services/config.rb
@@ -1,11 +1,3 @@
-###########################################
-# User Visible Rule Definitions
-###########################################
-
-# defines as the alert any EC2 instances that were launched more than 5 minutes ago
-# this set will be post-processed by the jsrunner below to examine the tags - nothing is directly
-# alerted on from this definition
-#
 coreo_aws_rule "ec2-get-all-instances-older-than" do
   action :define
   service :ec2
@@ -22,13 +14,17 @@ coreo_aws_rule "ec2-get-all-instances-older-than" do
   id_map "object.reservation_set.instances_set.instance_id"
 end
 
-###########################################
-# Compsite-Internal Resources follow until end
-#   (Resources used by the system for execution and display processing)
-###########################################
 
-# this resource simply executes the alert that was defined above
-#
+coreo_uni_util_variables "planwide" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.planwide.composite_name' => 'PLAN::stack_name'},
+                {'COMPOSITE::coreo_uni_util_variables.planwide.plan_name' => 'PLAN::name'},
+                {'COMPOSITE::coreo_uni_util_variables.planwide.results' => 'unset'},
+                {'COMPOSITE::coreo_uni_util_variables.planwide.number_violations' => 'unset'}
+            ])
+end
+
 coreo_aws_rule_runner_ec2 "advise-ec2-atk" do
   rules ["ec2-get-all-instances-older-than"]
   action :run
@@ -36,120 +32,15 @@ coreo_aws_rule_runner_ec2 "advise-ec2-atk" do
 end
 
 
-coreo_uni_util_jsrunner "jsrunner-process-suppression" do
-  action :run
-  provide_composite_access true
-  json_input '{"violations":COMPOSITE::coreo_aws_rule_runner_ec2.advise-ec2-atk.report}'
-  packages([
-               {
-                   :name => "js-yaml",
-                   :version => "3.7.0"
-               }       ])
-  function <<-EOH
-  const fs = require('fs');
-  const yaml = require('js-yaml');
-  let suppression;
-  try {
-      suppression = yaml.safeLoad(fs.readFileSync('./suppression.yaml', 'utf8'));
-  } catch (e) {
-  }
-  coreoExport('suppression', JSON.stringify(suppression));
-  const violations = json_input.violations;
-  const result = {};
-  let file_date = null;
-  const regionKeys = Object.keys(violations);
-  regionKeys.forEach(region => {
-      result[region] = {};
-      const violationKeys = Object.keys(violations[region]);
-      violationKeys.forEach(violator_id => {
-          result[region][violator_id] = {};
-          result[region][violator_id].tags = violations[region][violator_id].tags;
-          result[region][violator_id].violations = {};
-          const ruleKeys = Object.keys(violations[region][violator_id].violations);
-          ruleKeys.forEach(rule_id => {
-              let is_violation = true;
-              result[region][violator_id].violations[rule_id] = violations[region][violator_id].violations[rule_id];
-              const suppressionRuleKeys = Object.keys(suppression);
-              suppressionRuleKeys.forEach(suppress_rule_id => {
-                  const suppressionViolatorNum = Object.keys(suppression[suppress_rule_id]);
-                  suppressionViolatorNum.forEach(suppress_violator_num => {
-                      const suppressViolatorIdKeys = Object.keys(suppression[suppress_rule_id][suppress_violator_num]);
-                      suppressViolatorIdKeys.forEach(suppress_violator_id => {
-                          file_date = null;
-                          let suppress_obj_id_time = suppression[suppress_rule_id][suppress_violator_num][suppress_violator_id];
-                          if (rule_id === suppress_rule_id) {
-  
-                              if (violator_id === suppress_violator_id) {
-                                  const now_date = new Date();
-  
-                                  if (suppress_obj_id_time === "") {
-                                      suppress_obj_id_time = new Date();
-                                  } else {
-                                      file_date = suppress_obj_id_time;
-                                      suppress_obj_id_time = file_date;
-                                  }
-                                  let rule_date = new Date(suppress_obj_id_time);
-                                  if (isNaN(rule_date.getTime())) {
-                                      rule_date = new Date(0);
-                                  }
-  
-                                  if (now_date <= rule_date) {
-  
-                                      is_violation = false;
-  
-                                      result[region][violator_id].violations[rule_id]["suppressed"] = true;
-                                      if (file_date != null) {
-                                          result[region][violator_id].violations[rule_id]["suppressed_until"] = file_date;
-                                          result[region][violator_id].violations[rule_id]["suppression_expired"] = false;
-                                      }
-                                  }
-                              }
-                          }
-                      });
-                  });
-              });
-              if (is_violation) {
-  
-                  if (file_date !== null) {
-                      result[region][violator_id].violations[rule_id]["suppressed_until"] = file_date;
-                      result[region][violator_id].violations[rule_id]["suppression_expired"] = true;
-                  } else {
-                      result[region][violator_id].violations[rule_id]["suppression_expired"] = false;
-                  }
-                  result[region][violator_id].violations[rule_id]["suppressed"] = false;
-              }
-          });
-      });
-  });
-  
-  callback(result);
-  EOH
+coreo_uni_util_variables "update-planwide-1" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.planwide.results' => 'COMPOSITE::coreo_aws_rule_runner_ec2.advise-ec2-atk.report'},
+                {'COMPOSITE::coreo_uni_util_variables.planwide.number_violations' => 'COMPOSITE::coreo_aws_rule_runner_ec2.advise-ec2-atk.number_violations'},
+
+            ])
 end
 
-coreo_uni_util_jsrunner "jsrunner-process-table" do
-  action :run
-  provide_composite_access true
-  json_input '{"violations":COMPOSITE::coreo_aws_rule_runner_ec2.advise-ec2-atk.report}'
-  packages([
-               {
-                   :name => "js-yaml",
-                   :version => "3.7.0"
-               }       ])
-  function <<-EOH
-    var fs = require('fs');
-    var yaml = require('js-yaml');
-    try {
-        var table = yaml.safeLoad(fs.readFileSync('./table.yaml', 'utf8'));
-    } catch (e) {
-    }
-    coreoExport('table', JSON.stringify(table));
-    callback(table);
-  EOH
-end
-
-
-# this is doing the owner tag parsing only - it needs to also include the kill tag logic (and/or)
-#
 coreo_uni_util_jsrunner "tags-to-notifiers-array-ec2-atk" do
   action :run
   data_type "json"
@@ -284,7 +175,14 @@ callback(notifiers);
   EOH
 end
 
-# Send ec2-atk for email
+coreo_uni_util_variables "update-planwide-3" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.planwide.results' => 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-ec2-atk.JSONReport'},
+                {'COMPOSITE::coreo_uni_util_variables.planwide.table' => 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-ec2-atk.table'}
+            ])
+end
+
 coreo_uni_util_notify "advise-ec2-atk-to-tag-values" do
   action :${AUDIT_AWS_EC2_ATK_HTML_REPORT}
   notifiers 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-ec2-atk.return'
